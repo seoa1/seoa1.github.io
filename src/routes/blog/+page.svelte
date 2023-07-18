40500a3d3e0527f5fddb94a723a4199e621155d9
Andrew Seo <andrew.d.seo@gmail.com>
2023-07-17 22:28:03 -0700

    blog post

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,6 +1,5 @@
 <h1>This is my blog!</h1>
-<h2>I write some random thoughts here in raw html so they can get lost into the abyss!</h2>
-<p>My plan is to come back to these posts in a few years and laugh at how cringe I was when I was younger. But I guess YOU get the chance to laugh at me now!</p>
+<h2>I write random thoughts here!</h2>
 
 {#each data.summaries as {slug, title}}
     <a href="/blog/{slug}">{title}</a><br/>
--- src/routes/blog/blogPosts.js
+++ src/routes/blog/blogPosts.js
@@ -8,5 +8,12 @@ export const blogs = [
 		slug: 'real_post',
 		title: 'is this actually a post?',
 		content: "nope, it's just a test for new lines!\nlooks like they work!"
+	},
+	{
+		slug: 'what',
+		title: 'what should i write about?',
+		content: `what should i write about? i've never written in a blog before, and i've never been much of a talker.
+		why did i create a blog anyway? is a blog still a blog if no one reads it? maybe this is just a diary
+		that happens to be on a website. maybe i'll try writing poetry? or a story? i guess i have plenty of time to figure it out.`
 	}
 ];
